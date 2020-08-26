dcd747cb183cbda8b6f2f6afca3ef639677ff731
Devin <devdhera@gmail.com>
2020-08-26 21:07:32 +0530

    feat(features/home): home page features setup

---
+++ src/features/home/HomePageFeatures.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const HomePageFeatures = () => {
+    return (
+        <section className="section-content padding-y-sm">
+            <div className="container">
+                <article className="card card-body">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <figure className="item-feature">
+                                <span className="text-primary">
+                                    <i className="fa fa-2x fa-truck"></i>
+                                </span>
+                                <figcaption className="pt-3">
+                                    <h5 className="title">Fast delivery</h5>
+                                    <p>
+                                        Dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                                        ut labore{' '}
+                                    </p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div className="col-md-4">
+                            <figure className="item-feature">
+                                <span className="text-primary">
+                                    <i className="fa fa-2x fa-landmark"></i>
+                                </span>
+                                <figcaption className="pt-3">
+                                    <h5 className="title">Creative Strategy</h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div className="col-md-4">
+                            <figure className="item-feature">
+                                <span className="text-primary">
+                                    <i className="fa fa-2x fa-lock"></i>
+                                </span>
+                                <figcaption className="pt-3">
+                                    <h5 className="title">High secured </h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </section>
+    );
+};
+
+export default HomePageFeatures;
