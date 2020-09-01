d126509a90fc1db5414ee0ed5e6d656cb08f77e2
Devin <devdhera@gmail.com>
2020-09-01 17:13:38 +0530

    feat(features/product): changes product to use react star ratings

--- src/features/product/ProductCard.jsx
+++ src/features/product/ProductCard.jsx
@@ -2,6 +2,7 @@
 // modules
 import React from 'react';
 import { observer } from 'mobx-react-lite';
+import ReactStars from 'react-rating-stars-component';
 
 // utils
 import { formatCurrency } from '../../app/common/util/util';
@@ -21,24 +22,19 @@ const ProductCard = ({ product }) => {
                     {product.name}
                 </a>
 
-                <div className="rating-wrap">
-                    <ul className="rating-stars">
-                        <li style={{ width: '80%' }} className="stars-active">
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                        </li>
-                        <li>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                            <i className="fa fa-star"></i>
-                        </li>
-                    </ul>
-                    <span className="label-rating text-muted"> 34 reviws</span>
+                <div className="rating-wrap row">
+                    {product.averageRating && (
+                        <div className="col-md-7 pr-0">
+                            <ReactStars size={20} value={product.averageRating} edit={false} />
+                        </div>
+                    )}
+                    <div className={`${product.averageRating ? 'col-md-5 px-0 pt-1' : 'col-md-12 pl-2'}`}>
+                        <span className="label-rating text-muted">
+                            {' '}
+                            {product.reviewCount}{' '}
+                            {!product.reviewCount ? 'Not Reviewed Yet' : product.reviewCount > 1 ? 'reviews' : 'review'}
+                        </span>
+                    </div>
                 </div>
                 <div className="price mt-1">{formatCurrency(product.unitPrice)}</div>
             </figcaption>
