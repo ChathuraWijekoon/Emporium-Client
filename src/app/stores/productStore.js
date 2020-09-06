c874ad59f0c0132d020b9e881ea8a17fe737b6db
Devin <devdhera@gmail.com>
2020-09-07 01:45:08 +0530

    feat(stores/productstore): adds create, update and photo upload store actions

--- src/app/stores/productStore.js
+++ src/app/stores/productStore.js
@@ -1,5 +1,7 @@
 import { observable, action, computed, runInAction, reaction, decorate, toJS } from 'mobx';
+import { toast } from 'react-toastify';
 import agent from '../api/agent';
+import { history } from '../..';
 
 const LIMIT = 4;
 
@@ -13,7 +15,7 @@ class ProductStore {
             () => {
                 this.page = 0;
                 this.productRegistry.clear();
-                this.loadProducts();
+                // this.loadProducts();
             },
         );
     }
@@ -27,18 +29,27 @@ class ProductStore {
     productCount = 0;
     page = 0;
     predicate = new Map();
+    loadAll = false;
 
     setPredicate = (predicate, value) => {
         this.predicate.clear();
         if (predicate !== 'all') {
             this.predicate.set(predicate, value);
+        } else {
+            this.loadAll = value;
         }
+    };
+
+    deletePredicate = () => {
+        this.predicate.clear();
     };
 
     get axiosParams() {
         const params = new URLSearchParams();
-        params.append('limit', String(LIMIT));
-        params.append('page', `${this.page ? this.page * LIMIT : 0}`);
+        if (!this.loadAll) {
+            params.append('limit', String(LIMIT));
+            params.append('page', `${this.page ? this.page * LIMIT : 1}`);
+        }
         this.predicate.forEach((value, key) => {
             params.append(key, value);
         });
@@ -53,7 +64,7 @@ class ProductStore {
         return Array.from(this.productRegistry.values());
     }
 
-    setPage = (page: number) => {
+    setPage = (page) => {
         this.page = page;
     };
 
@@ -104,6 +115,64 @@ class ProductStore {
     getProduct = (id) => {
         return this.productRegistry.get(id);
     };
+
+    createProduct = async (categoryId, product) => {
+        this.submitting = true;
+        try {
+            const response = await agent.Products.create(product, categoryId);
+            runInAction('create product', () => {
+                this.productRegistry.set(response.data._id, response.data);
+                this.submitting = false;
+            });
+            history.push(`/sell/product/${response.data._id}`);
+        } catch (error) {
+            runInAction('create product error', () => {
+                this.submitting = false;
+            });
+            toast.error(error.data?.error);
+            console.log(error.response);
+        }
+    };
+
+    editProduct = async (product) => {
+        this.submitting = true;
+        try {
+            await agent.Products.update(product);
+            runInAction('editing product', () => {
+                this.productRegistry.set(product._id, product);
+                this.product = product;
+                this.submitting = false;
+            });
+            // history.push(`/admin/product/${product._id}`);
+            toast.success('Successfully updated product');
+        } catch (error) {
+            runInAction('edit product error', () => {
+                this.submitting = false;
+            });
+            toast.error(error.data.error);
+        }
+    };
+
+    uploadProductPhoto = async (product, file) => {
+        this.submitting = true;
+        try {
+            const response = await agent.Products.uploadPhoto(product._id, file);
+            runInAction('uploading product photo', () => {
+                this.productRegistry.set(product._id, product);
+                this.product.photo = response.data;
+                this.submitting = false;
+            });
+            // history.push(`/admin/product/${product._id}`);
+            window.location.reload();
+            toast.success('Successfully uploaded product photo');
+        } catch (error) {
+            runInAction('uploading product photo error', () => {
+                this.submitting = false;
+            });
+            toast.error(error.data.error);
+            console.log(error);
+        }
+    };
 }
 
 decorate(ProductStore, {
@@ -121,9 +190,13 @@ decorate(ProductStore, {
     totalPages: computed,
 
     setPredicate: action,
+    deletePredicate: action,
     setPage: action,
     loadProducts: action,
     loadProduct: action,
+    createProduct: action,
+    editProduct: action,
+    uploadProductPhoto: action,
 });
 
 export default ProductStore;
