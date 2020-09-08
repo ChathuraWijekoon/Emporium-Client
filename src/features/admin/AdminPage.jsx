a3f2a7b2bde34d7181fabd2a4b4188c0454ab13a
Devin <devdhera@gmail.com>
2020-09-09 02:23:07 +0530

    feat(features/admin): adds admin orders table

--- src/features/admin/AdminPage.jsx
+++ src/features/admin/AdminPage.jsx
@@ -9,23 +9,27 @@ import { RootStoreContext } from '../../app/stores/rootStore';
 import AdminCounts from './AdminCounts';
 import AdminProductsTable from './AdminProductsTable';
 import AdminUsersTable from './AdminUsersTable';
+import AdminOrdersTable from './AdminOrdersTable';
 
 const AdminPage = () => {
     const rootStore = useContext(RootStoreContext);
     const { loadProducts, loadUsers } = rootStore.adminStore;
+    const { loadOrders } = rootStore.orderStore;
 
     const [activeTable, _setActiveTable] = useState('products');
 
     useEffect(() => {
         loadProducts();
         loadUsers();
-    }, [loadProducts, loadUsers]);
+        loadOrders();
+    }, [loadProducts, loadUsers, loadOrders]);
 
     return (
         <div className="container mt-3">
             <AdminCounts setActiveTable={_setActiveTable} />
             {activeTable === 'products' && <AdminProductsTable />}
             {activeTable === 'users' && <AdminUsersTable />}
+            {activeTable === 'orders' && <AdminOrdersTable />}
         </div>
     );
 };
